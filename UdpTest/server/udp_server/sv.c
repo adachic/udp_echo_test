20e457cc094af772dd0dcb540ba7454b208243ba
安達 彰典 <akinori_adachi@dwango.co.jp>
2012-08-30 18:54:41 +0900

    udp受信後、tcpでつなぐ

--- UdpTest/server/udp_server/sv.c
+++ UdpTest/server/udp_server/sv.c
@@ -10,6 +10,44 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+
+void tcpconnect(struct sockaddr_in from, char *ipstr)
+{
+ struct sockaddr_in server;
+ int sock;
+ char buf[32];
+ int n;
+
+ /* ソケットの作成 */
+ sock = socket(AF_INET, SOCK_STREAM, 0);
+
+ /* 接続先指定用構造体の準備 */
+ server.sin_family = AF_INET;
+ server.sin_port = htons(from.sin_port);
+
+ /* 127.0.0.1はlocalhost */
+ inet_pton(AF_INET, ipstr, &server.sin_addr.s_addr);
+
+ printf("willconnect\n");
+ printf("recv data from : %d\n",  ntohs(from.sin_port));
+ /* サーバに接続 */
+ // int ret =connect(sock, (struct sockaddr *)&server, sizeof(server));
+ int ret = connect(sock, (struct sockaddr *)&from, sizeof(from));
+ printf("ret:%d\n",ret);
+
+ /* サーバからデータを受信 */
+ memset(buf, 0, sizeof(buf));
+ printf("read\n");
+ n = read(sock, buf, sizeof(buf));
+
+ printf("%d, %s\n", n, buf);
+
+ /* socketの終了 */
+ close(sock);
+
+ return 0;
+}
+
 int
 main()
 {
@@ -30,9 +68,9 @@ main()
 
  bind(sock, (struct sockaddr *)&addr, sizeof(addr));
 
- printf("waiting for recv..\n");
  while(1){
   memset(buf, 0, sizeof(buf));
+  printf("waiting for recv..\n");
 
   /*蜿嶺ｿ｡*/
   size = recvfrom(sock, buf, 2048-1, 0, 
@@ -42,6 +80,8 @@ main()
   inet_ntop(AF_INET, &from.sin_addr, str, sizeof(str));
   printf("%s\n",buf);
   printf("recv data from : %s:%d\n", str, ntohs(from.sin_port));
+
+  tcpconnect(from,str);
 
   /*霑比ｿ｡*/
   strcpy(buf , "echo");
